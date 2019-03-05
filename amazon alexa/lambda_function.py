6cc4061a37ff7b4b869cbbd870fcfba4dcc5655b
Niladri Shekhar Dutt <niladrishekhardutt@gmail.com>
2019-03-04 20:10:52 -0800

    Update lambda_function.py

--- amazon alexa/lambda_function.py
+++ amazon alexa/lambda_function.py
@@ -1,18 +1,6 @@
-"""
-This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
-The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well
-as testing instructions are located at http://amzn.to/1LzFrj6
-
-For additional samples, visit the Alexa Skills Kit Getting Started guide at
-http://amzn.to/1LGWsLG
-"""
-
-
-
-
 from __future__ import print_function
 
-save_var=""
+
 # --------------- Helpers that build all of the responses ----------------------
 
 def build_speechlet_response(title, output, reprompt_text, should_end_session):
@@ -72,11 +60,7 @@ def handle_session_end_request():
         card_title, speech_output, None, should_end_session))
 
 
-def create_favorite_color_attributes(favorite_color):
-    return {"favoriteColor": favorite_color}
-
-
-def set_color_in_session(intent, session):
+def who_is_the_friend(intent, session):
     """ Sets the color in the session and prepares the speech to reply to the
     user.
     """
@@ -126,26 +110,6 @@ def what_is_the_committDate(intent,session):
         card_title, speech_output, reprompt_text, should_end_session))
 
 
-def get_color_from_session(intent, session):
-    session_attributes = {}
-    reprompt_text = None
-
-    if session.get('attributes', {}) and "favoriteColor" in session.get('attributes', {}):
-        favorite_color = session['attributes']['favoriteColor']
-        speech_output = "Your favorite color is " + favorite_color + \
-                        ". Goodbye."
-        should_end_session = True
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "You can say, my favorite color is red."
-        should_end_session = False
-
-    # Setting reprompt_text to None signifies that we do not want to reprompt
-    # the user. If the user does not respond or says something that is not
-    # understood, the session will end.
-    return build_response(session_attributes, build_speechlet_response(
-        intent['name'], speech_output, reprompt_text, should_end_session))
-
 
 # --------------- Events ------------------
 
@@ -178,7 +142,7 @@ def on_intent(intent_request, session):
 
     # Dispatch to your skill's intent handlers
     if intent_name == "MakeaBet":
-        return set_color_in_session(intent, session)
+        return who_is_the_friend(intent, session)
     elif intent_name == "Bets":
         return what_is_the_bet(intent, session)
     elif intent_name == "Stake":
