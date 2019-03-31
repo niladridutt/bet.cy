0e545173c28d913d6a435fabc8421e402bfa3e12
Niladri Shekhar Dutt <niladrishekhardutt@gmail.com>
2019-03-30 19:53:02 -0700

    Update lambda_function.py

--- amazon alexa/lambda_function.py
+++ amazon alexa/lambda_function.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
-
+import json
+from botocore.vendored import requests
 
 # --------------- Helpers that build all of the responses ----------------------
 
@@ -41,7 +42,7 @@ def get_welcome_response():
 
     session_attributes = {}
     card_title = "Welcome"
-    speech_output = "Welcome to the Betcy's Alexa Skill. Who do you want to make a bet with?"
+    speech_output = "Welcome to Carbon's Alexa Skill. Let's build a more sustainable environment."
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
     reprompt_text = "Please make a bet with your friend."
@@ -52,27 +53,30 @@ def get_welcome_response():
 
 def handle_session_end_request():
     card_title = "Session Ended"
-    speech_output = "Thank you for trying the Alexa Skills Kit sample. " \
-                    "Have a nice day! "
+    speech_output = "Thank you for using Betcy's Alexa skill. Have a nice day! "
     # Setting this to true ends the session and exits the skill.
     should_end_session = True
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
 
 
-def who_is_the_friend(intent, session):
-    """ Sets the color in the session and prepares the speech to reply to the
-    user.
-    """
+
+def who_is_the_receiver(intent, session):
 
     card_title = intent['name']
     session_attributes = {}
     should_end_session = False
     name=intent['slots']['tofriend']['value']
-    save_var=name
     speech_output = "I will create a smart contract between you and "+ name +". What is your bet?"
     reprompt_text = "I will create a smart contract between you and "+ name +". What is your bet?"
-    
+    url = "https://www.jsonstore.io/54298701bfa16f8c77653c87b9008c3f3a681c74ca82c59649efc9159f910e46"
+    pay={'receiver': name}
+    headers = {
+        'Content-Type': "application/json",
+        'cache-control': "no-cache",
+        'Accept': 'text/plain'
+    }
+    response = requests.request("POST", url, data=json.dumps(pay), headers=headers)
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
         
@@ -84,6 +88,14 @@ def what_is_the_bet(intent,session):
     name=intent['slots']['betmade']['value']
     speech_output = "Your bet is on "+ name +". What is your wager?"
     reprompt_text = "Your bet is on "+ name +". What is your wager?"
+    url="https://www.jsonstore.io/f69f67573b1097131a3d0dd3238f05c01eb95d943adc8ec173d9b903f0989419"
+    pay={'receiver': name}
+    headers = {
+        'Content-Type': "application/json",
+        'cache-control': "no-cache",
+        'Accept': 'text/plain'
+    }
+    response = requests.request("POST", url, data=json.dumps(pay), headers=headers)
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
         
@@ -95,6 +107,12 @@ def what_is_the_wager(intent,session):
     name=intent['slots']['productid']['value']
     speech_output = "Your wager is Macy's product with web id "+ str(name) +". When is your commit date?"
     reprompt_text = "Your wager is Macy's product with web id "+ str(name) +". When is your commit date?"
+    url = "https://www.jsonstore.io/cc18fd5358601223f518adc547cb765a84d85d34fbdfe3466b55d6e454f29bf1"
+    headers = {
+        'Content-Type': "application/json",
+        'cache-control': "no-cache"
+    }
+    response = requests.request("POST", url, data=name, headers=headers)
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
@@ -104,8 +122,8 @@ def what_is_the_committDate(intent,session):
     session_attributes = {}
     should_end_session = False
     name=intent['slots']['committDate']['value']
-    speech_output = "Your bet is due on "+ name +". I'm deploying it on a smart contract."
-    reprompt_text = "Your bet is due on "+ name +". I'm deploying it on a smart contract."
+    speech_output = "Your bet is due on "+ name +". I'm deploying it on the Ethereum Blockchain."
+    reprompt_text = "Your bet is due on "+ name +". I'm deploying it on the Ethereum Blockchain."
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
@@ -142,7 +160,7 @@ def on_intent(intent_request, session):
 
     # Dispatch to your skill's intent handlers
     if intent_name == "MakeaBet":
-        return who_is_the_friend(intent, session)
+        return who_is_the_receiver(intent, session)
     elif intent_name == "Bets":
         return what_is_the_bet(intent, session)
     elif intent_name == "Stake":
@@ -195,10 +213,3 @@ def lambda_handler(event, context):
         return on_intent(event['request'], event['session'])
     elif event['request']['type'] == "SessionEndedRequest":
         return on_session_ended(event['request'], event['session'])
-    
-    if event['request']['type'] == "BlockchainRequest":
-        return {
-       "Person": "John",
-       "Bet": "I will not drink",
-       "Wage": "6363"
-   }
